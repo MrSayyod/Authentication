7fcc8822806cebaa80459f305073ecd71d7a2017
calvinoea <calvinebunamu@gmail.com>
2020-06-26 14:27:27 +0100

    Author and user models, views, and controls added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+  resources :posts, only: [:create, :new, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ root to: "posts#index"
+
+ devise_for :user
+
+
 end
