979f0d12390bb86465a7e1a03a9597e95ad7c5b2
MrSayyod <ssayyod@gmail.com>
2020-07-13 18:38:58 +0500

    Makes changes that only author of post can edit or delete the post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[show edit update destroy]
-  before_action :authenticate_user!, only: %i[new create]
+  before_action :authenticate_user!, only: %i[new create edit update destroy]
 
   # GET /posts
   # GET /posts.json
